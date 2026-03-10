d1d7af7684ab01f5a9640f2a554e0f3a7f075409
srilupalepu <sri.pvrk@gmail.com>
2026-03-10 18:26:59 +0530

    Initial commit with CI workflow

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express();
 
 const PORT = 3000;
 
-const name = "srilakshmi"
-const unusednum = 5
-console.log(name)
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, My Name is Srilu 👋</h1>');
